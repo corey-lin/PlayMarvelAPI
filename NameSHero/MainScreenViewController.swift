7e2c57355d86fc72eabc14aac0b64034f5575c33
Corey Lin <lyfone@gmail.com>
2016-03-02 13:36:37 -0800

    create a Quiz screen add some fake contents for layout purpose

--- NameSHero/MainScreenViewController.swift
+++ NameSHero/MainScreenViewController.swift
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainScreenViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    self.navigationController?.navigationBarHidden = true;
   }
 
   override func didReceiveMemoryWarning() {
---
+++ NameSHero/QuizViewController.swift
@@ -0,0 +1,8 @@
+//
+//  QuizViewController.swift
+//  NameSHero
+//
+//  Created by coreylin on 3/2/16.
+//  Copyright © 2016 coreylin. All rights reserved.
+//
+
